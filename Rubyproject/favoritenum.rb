9aec2422e22afc7ad222fb76b304a063a77ef0f6
toykira <31515870+toykira@users.noreply.github.com>
2017-08-31 23:39:19 +0700

    Add files via upload

---
+++ Rubyproject/favoritenum.rb
@@ -0,0 +1,4 @@
+puts 'Hello there, Can you tell me your favourite number?'
+num = gets.to_i
+puts "Your favourite number is #{num}?"
+puts "Well its not bad but  #{num * 10} is literally 10 times better!"
